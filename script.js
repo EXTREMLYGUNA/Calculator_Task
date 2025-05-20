604d87cd79133e0bab65dea31c55d15453e15b93
GUNASEKAR <gunasekar17329777557amma@gmail.com>
2025-05-20 16:56:03 +0530

    Online-resume

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let div = document.createElement("div");
+var div = document.createElement("div");
 div.innerHTML =
- `<div class="container">
- <div class="calculator">
+ `<div className="container">
+ <div id='calculator'>
      <input type="text" placeholder="0  " id="output-screen">
-     <button onclick="Clear()" id="clear">Cl</button>
+     <button onclick="Clear()" id='clear'>CL</button>
      <button onclick="del()">DEL</button>
      <button onclick="display('%')">%</button>
      <button onclick="display('/')">/</button>
@@ -11,18 +11,19 @@ div.innerHTML =
      <button onclick="display('8')">8</button>
      <button onclick="display('9')">9</button>
      <button onclick="display('*')">*</button>
-     <button onclick="display('4')">4</button>
+     <button onclick="display('4')">4</button> 
      <button onclick="display('5')">5</button>
      <button onclick="display('6')">6</button>
-     <button onclick="display('-')" id="subtract">-</button>
-     <button onclick="display('1')">1</button>
-     <button onclick="display('2')">2</button>
+     <button onclick="display('-')" id='subtract'>-</button>
+     <button onclick="display('1')" id='one'>1</button>
+     <button onclick="display('2')" id='two'>2</button>
      <button onclick="display('3')">3</button>
-     <button onclick="display('+')" id="add">+</button>
+     <button onclick="display('+')" id='add'>+</button>
      <button onclick="display('.')">.</button>
      <button onclick="display('0')">0</button>
-     <button onclick="Calculate()" id="equal">=</button>
- </div>
+     <button onclick="Calculate(result)" id='equal'>=</button>
+     
+ </div> 
 </div>`
 document.body.append(div);
 let outputScreen = document.getElementById("output-screen");
@@ -33,11 +34,9 @@ function display(num){
 function Calculate(){
     try{
         outputScreen.value = eval(outputScreen.value);
-
     }
     catch(err){
         alert("Invalid");
-
     }
 }
 function Clear(){
@@ -46,3 +45,12 @@ function Clear(){
 function del(){
     outputScreen.value = outputScreen.value.slice(0, -1);
 }
+document.querySelector('#clear').click();
+var one = document.getElementById('1');
+var two = document.getElementById('2');
+var add = document.getElementById('add');
+var result = document.getElementById('result');
+one.click();
+two.click();
+add.click();
+document.getElementById('equal').click();
